617c6381bee2df14f478193cd94696c91790a4ff
Sergio <sergio.leiva@sngular.com>
2021-09-28 12:35:29 -0400

    Contact form saving information

--- complex_web_server/server.py
+++ complex_web_server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 print(__name__)
 
@@ -14,18 +14,15 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
-# @app.route('/about.html')
-# def about():
-#     return render_template('about.html')
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        file = database.write(f'\n{data["email"]},{data["subject"]},{data["message"]}')
 
-# @app.route('/works.html')
-# def works():
-#     return render_template('works.html')
-
-# @app.route('/contact.html')
-# def contact():
-#     return render_template('contact.html')
-
-# @app.route('/favicon.ico')
-# def blog():
-#     return 'These are my thoughts on blogs'
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('/thankyou.html')
+    else:
+        return "Something went wrong!"
